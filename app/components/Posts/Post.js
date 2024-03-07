301870d44993856a9622d14b4a7203dbc6a5bd74
stazgmina <stazgmina@gmail.com>
2024-03-07 21:52:19 +0100

    post ui change

--- app/components/Posts/Post.js
+++ app/components/Posts/Post.js
@@ -6,24 +6,24 @@ const Post = ({ id, title, description, content, category, likeCount, author, da
   const categoryColors = {
     Technology: 'bg-blue-300',
     Politics: 'bg-yellow-300',
-    Enviroment: 'bg-green-300',
+    Environment: 'bg-green-300',
     Art: 'bg-red-300',
   }
 
   return (
-    <article className='overflow-hidden rounded-md shadow-xl max-w-[300px] hover:scale-110 transition cursor-pointer'>
+    <article className='overflow-hidden rounded-md shadow-xl max-w-[300px] hover:scale-110 transition cursor-pointer flex flex-col'>
         <img src={image} />
-        <section className="flex flex-col items-start gap-2 p-4">
+        <section className="flex flex-col items-start flex-1 gap-2 p-4">
             <p className={`px-2 text-white rounded-md ${categoryColors[category]}`}>
                 {category}
             </p>
-            <h1 className='text-xl'>
+            <h1 className='text-xl line-clamp-2'>
                 {title}
             </h1>
-            <p className='text-sm text-gray-400'>
+            <p className='flex-1 text-sm text-gray-400 line-clamp-3'>
                 {description}
             </p>
-            <section className='flex items-end justify-between w-full'>
+            <section className='flex items-end self-end justify-between w-full'>
                 <div className='flex gap-2'>
                     <img src={author.image || '/assets/userPlaceholder.png'} className='rounded-md w-[50px] h-[50px] object-cover'/>
                     <div>
